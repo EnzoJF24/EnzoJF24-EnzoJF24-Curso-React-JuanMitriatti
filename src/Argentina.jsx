fb2d5442fa26e788f391d5aaa9ef432227db5160
EnzoJF24 <enzofulcheri@gmail.com>
2023-01-26 16:56:39 -0300

    primer ejercicio react (ej 4)

---
+++ src/Argentina.jsx
@@ -0,0 +1,19 @@
+import React from "react";
+
+class Argentina extends React.Component{
+    
+    
+    render(){
+        const nombre= "enzo"
+        const apellido= "fulcheri"
+        return(
+            <div>
+                <h1>{nombre}</h1>
+                <h1>{apellido}</h1>
+            </div>
+        );
+    }
+
+}
+ 
+export default Argentina;
